29b926c5b8b4394099967019258ee009ed4fb9f0
Discusser <noobly321growing@gmail.com>
2024-02-17 10:38:16 +0100

    Add possibility for multiple files, color toggle and transparency toggle

--- imgtoascii.cpp
+++ imgtoascii.cpp
@@ -5,23 +5,38 @@
 
 std::vector<char> character_list = {'@', 'J', 'D', '%', '*', 'P', '+', 'Y', '$', ',', '.'};
 
-gchar *img_path;
+char **img_paths;
 int max_width = 384;
 int max_height = 384;
+bool transparent_background = false;
+int image_spacing = 8;
+bool no_colors = false;
 
 GOptionEntry command_line_arguments[] = {
-    {"file", 'f', G_OPTION_FLAG_NONE, G_OPTION_ARG_FILENAME, &img_path,
-     "The path to the image to be converted to ASCII", ""},
+    {"file", 'f', G_OPTION_FLAG_NONE, G_OPTION_ARG_FILENAME_ARRAY, &img_paths,
+     "The path to the image to be converted to ASCII. To set multiple files, repeat the argument", ""},
     {"max_height", 'h', G_OPTION_FLAG_NONE, G_OPTION_ARG_INT, &max_height, "Maximum height of the image in px"},
     {"max_width", 'w', G_OPTION_FLAG_NONE, G_OPTION_ARG_INT, &max_width, "Maximum width of the image in px"},
+    {"transparent_background", 't', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &transparent_background,
+     "Whether or not the background should be transparent on images with alpha channels"},
+    {"image_spacing", 's', G_OPTION_FLAG_NONE, G_OPTION_ARG_INT, &image_spacing,
+     "The spacing between images when printing multiple images at the same time"},
+    {"no_colors", 0, G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &no_colors, "Disables colors"},
     {nullptr},
 };
 
 int main(int argc, char *argv[]) {
   parse_command_line_arguments(&argc, &argv);
 
-  Image image = read_image(img_path);
-  print_image(image);
+  std::vector<std::string> paths;
+  int i = 0;
+  while (img_paths[i]) {
+    paths.push_back(img_paths[i]);
+    i++;
+  }
+
+  std::vector<Image> images = read_images(paths);
+  print_images(images, image_spacing);
 
   return 0;
 }
@@ -48,6 +63,17 @@ Image read_image(std::string image_path) {
   return image;
 }
 
+std::vector<Image> read_images(std::vector<std::string> image_paths) {
+  std::vector<Image> images;
+  images.reserve(image_paths.size());
+
+  for (std::string path : image_paths) {
+    images.push_back(read_image(path));
+  }
+
+  return images;
+}
+
 void parse_command_line_arguments(int *argc, char **argv[]) {
   GError *error = nullptr;
   GOptionContext *context = g_option_context_new(nullptr);
@@ -61,37 +87,59 @@ void parse_command_line_arguments(int *argc, char **argv[]) {
 
   g_option_context_free(context);
 
-  if (!img_path) {
+  if (!img_paths) {
     g_print("Missing image path, specify it with -f or --file\n");
   }
 }
 
-void print_image(Image image) {
-  for (int j = 0; j < image.get_height(); j++) {
-    for (int i = 0; i < image.get_width(); i++) {
-      const unsigned char *pixel_offset = image.get_data() + (i + image.get_width() * j) * image.get_num_channels();
-
-      unsigned char grayscale = 0;
-      unsigned char rgb[] = {0, 0, 0};
-      for (int k = 0; k < image.get_num_channels(); k++) {
-        grayscale += pixel_offset[k];
-        if (k < static_cast<int>(sizeof(rgb))) {
-          rgb[k] = pixel_offset[k];
-        }
+void print_images(std::vector<Image> images, int spacing) {
+  std::string string_spacing(spacing, ' ');
+  int max_height = 0;
+  for (Image image : images) {
+    max_height = std::max(image.get_height(), max_height);
+  }
+  for (int j = 0; j < max_height; j++) {
+    for (Image image : images) {
+      if (j < image.get_height()) {
+        print_image_row(image, j);
+        g_print("%s", string_spacing.data());
       }
-      grayscale /= image.get_num_channels();
-
-      char character = character_list[static_cast<int>(grayscale / 255.0 * character_list.size())];
-      set_terminal_background(rgb[0], rgb[1], rgb[2]);
-      g_print("%c%c", character, character);
-      clear_terminal_background();
     }
     g_print("\n");
   }
 }
 
-void set_terminal_background(int r, int g, int b) {
-  g_print("\x1b[48;2;%i;%i;%im", r, g, b);
+void print_image_row(Image image, int row) {
+  for (int i = 0; i < image.get_width(); i++) {
+    const unsigned char *pixel_offset = image.get_data() + (i + image.get_width() * row) * image.get_num_channels();
+
+    unsigned char grayscale = 0;
+    unsigned char rgb[] = {0, 0, 0, 1};
+    for (int k = 0; k < image.get_num_channels(); k++) {
+      if (k < 3)
+        grayscale += pixel_offset[k];
+      if (k < static_cast<int>(sizeof(rgb))) {
+        rgb[k] = pixel_offset[k];
+      }
+    }
+    grayscale /= image.get_num_channels();
+
+    char character = character_list[static_cast<int>(grayscale / 255.0 * character_list.size())];
+    if (!no_colors)
+      set_terminal_background(rgb[0], rgb[1], rgb[2], rgb[3]);
+    if (rgb[3] == 0 && transparent_background)
+      character = ' ';
+    g_print("%c%c", character, character);
+    if (!no_colors)
+      clear_terminal_background();
+  }
+}
+
+void set_terminal_background(int r, int g, int b, int a) {
+  if (a == 0)
+    clear_terminal_background();
+  else
+    g_print("\x1b[48;2;%i;%i;%im", r, g, b);
 }
 
 void clear_terminal_background() {
--- imgtoascii.hpp
+++ imgtoascii.hpp
@@ -11,6 +11,8 @@ extern std::vector<char> character_list;
 
 void clear_terminal_background();
 Image read_image(std::string image_path);
-void set_terminal_background(int r, int g, int b);
+std::vector<Image> read_images(std::vector<std::string> image_paths);
+void set_terminal_background(int r, int g, int b, int a);
 void parse_command_line_arguments(int *argc, char **argv[]);
-void print_image(Image image);
+void print_images(std::vector<Image> images, int spacing);
+void print_image_row(Image image, int row);
